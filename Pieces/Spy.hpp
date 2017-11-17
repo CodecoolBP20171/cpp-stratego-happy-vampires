6ebab401b73df1a5c4c9ee5be60c5fb2124b986d
Daniel Eros <drerosdaniel@gmail.com>
2017-11-17 11:58:32 +0100

    spy defeats marshall

--- Pieces/Spy.hpp
+++ Pieces/Spy.hpp
@@ -12,9 +12,16 @@ class Spy : public Soldier {
 public:
     Spy(int x, int y, Rank rank, Color color, SDL_Texture *faceUpTexture, SDL_Texture *backGroundTexture,
         bool toBoard, bool isFaceDown) : Soldier(x, y, rank, color, faceUpTexture, backGroundTexture, toBoard, isFaceDown){}
-    FightWinner attack(std::shared_ptr<Piece> piece) override {
+    FightWinner attack(std::shared_ptr<Piece> defender) override {
         // TODO: Spy can only defeat the marshall and the flag; draws with a spy
         std::cout << "Spy attacks" << std::endl;
+        if(this->getRank() == defender->getRank()){
+            return FightWinner::draw;
+        }
+        if(defender->getRank() == Rank::marshallRank){
+            return FightWinner::attacker;
+        }
+        return (this->getRank() > defender->getRank() ? FightWinner::attacker : FightWinner::defender);
     }
 };
 
